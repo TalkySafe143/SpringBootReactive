9126c910df9b62ae2e1523cf6b4a2553c9775506
TalkySafe143 <sebastian.galindo143@gmail.com>
2023-10-30 09:26:36 -0500

    Initial commit

---
+++ src/main/java/edu/javeriana/react3/React3Application.java
@@ -0,0 +1,13 @@
+package edu.javeriana.react3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class React3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(React3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/javeriana/react3/React3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.javeriana.react3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class React3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
